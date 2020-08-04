a79e3bcfb52b4413934e74e4375a533dbd76225e
Mahendra Kumar <m.kumar@thesynapses.com>
2020-08-04 00:01:06 +0000

    change in port

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const Stream = require('./node-rtsp-stream');
-const streamUrl = 'rtsp://mitesh:ils@5533@103.9.13.148:554/cam/realmonitor?channel=7&subtype=0';
+//const Stream1 = require('./node-rtsp-stream');
+//const streamUrl = 'rtsp://170.93.143.139/rtplive/470011e600ef003a004ee33696235daa';
+//const streamUrl1 = 'rtsp://mitesh:ils@5533@103.9.13.148:554/cam/realmonitor?channel=3&subtype=0';
+const port = process.env.PORT;
+const url = process.env.URL;
+
+console.log(port);
+console.log(url);
 
 stream = new Stream({
   name: 'foscam_stream',
-  streamUrl: streamUrl,
-  wsPort: 4443
+  streamUrl: url,
+  wsPort: port
 });
+
+
+
